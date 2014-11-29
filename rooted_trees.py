625c9c182f736ef828fcdc47b15a5b2e6ad8c687
Caleb Levy <clevy@Calebs-MacBook-Pro.local>
2014-11-28 18:59:51 -0800

    First commit to repo on functional equations. Finally found the paper on rooted trees.

---
+++ rooted_trees.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# 
+"""
+Contains  one main  function:  rooted_trees. Takes  an integer  N  as input  and
+outputs a generator object enumerating  all isomorphic unlabeled rooted trees on
+N nodes. Consult OEIS A000055 to find len(list(rooted_trees(N))).
+
+Copyright (C) 2014 Caleb Levy - All Rights Reserved.
+The terms  of non-commercial usage of  this code are simply  providing credit of
+some variety,  either in the  typical list of  contributors section of  the code
+repository or,  if used for an  academic paper, some contribution  in the paper.
+For commercial use, please contact me at caleb.levy@[berkeley.edu, gmail.com].
+"""
+def successor(L):
+    N = len(L)
+    p = N-1
+    while L[p] == L[1]:
+        p -= 1
+    q = p-1
+    while L[q] >= L[p]:
+        q -= 1
+    for I in range(p,N):
+        L[I] = L[I-(p-q)]
+
+def rooted_trees(N):
+    L = [I+1 for I in range(N)]
+    yield L
+    while L[1] != L[2]:
+        successor(L)
+        yield L
