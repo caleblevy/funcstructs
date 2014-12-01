813da9b5e240fb540f40eff75512edd2c08536ec
Caleb Levy <clevy@Calebs-MacBook-Pro.local>
2014-11-30 22:21:30 -0800

    Commited rooted trees, though now silly and useless (ish).

--- rooted_trees.py
+++ rooted_trees.py
@@ -11,7 +11,7 @@ repository or,  if used for an  academic paper, some contribution  in the paper.
 For commercial use, please contact me at caleb.levy@berkeley.edu.
 """
 from eppstein.IntegerPartitions import partitions
-from itertools import combinations_with_replacement
+from itertools import combinations_with_replacement, product
 
 def successor(L):
     N = len(L)
@@ -25,14 +25,23 @@ def successor(L):
         L[I] = L[I-(p-q)]
 
 def rooted_trees(N,max_tree=None):
+    if N == 0:
+        yield 0
+        return
+    elif N == 1:
+        yield (1,)
+        return
+    elif N == 2:
+        yield (1,2)
+        return
     L = [I+1 for I in range(N)]
     yield tuple(L)
-    if max_tree == L:
+    if max_tree == tuple(L):
         return
     while L[1] != L[2]:
         successor(L)
         yield tuple(L)
-        if max_tree == L:
+        if max_tree == tuple(L):
             return
             
 def split_set(partition):
@@ -54,21 +63,30 @@ def unsplit_set(y, d):
     packed_list = [[y[I]]*d[I] for I in range(len(y))]
     return unpack(packed_list)
     
-def 
+def partition_forests(partition):
+    y, d = split_set(partition)
+    l = len(y)
+    trees = [tuple(rooted_trees(I)) for I in y]
+    seeds = [combinations_with_replacement(trees[I],d[I]) for I in range(l)]
+    seeds = [list(seed) for seed in seeds]
+    for forest in product(*seeds):
+        yield tuple(unpack(forest))
+        
+def forests(n):
+    for partition in partitions(n):
+        for forest in partition_forests(partition):
+            yield forest
     
 a = [1,1,1,1,2,1,4,3,3,2]
 y, d = split_set(a)
-print y
-print d
 aa = unsplit_set(y,d)
-print aa
 
-def forests(N):
+def forests4(N):
     for I in rooted_trees(N):
         for J in rooted_trees(N,max_tree=I):
             for K in rooted_trees(N,max_tree=J):
-                for L in rooted_trees(N,max_tree=K):
-                    yield [I,J,K,L]
+                # for L in rooted_trees(N,max_tree=K):
+                    yield tuple([I,J,K])#,L]
                     
 def forests2(p4):
     for I in rooted_trees(p4[0]):
@@ -80,26 +98,10 @@ def forests2(p4):
 def forests3(p4):
     if len(p4) != 1:
         f = [forests3(p4[:-1])].append()
-    
-    
-
-if __name__ == '__main__':
-    for I in range(3,15):
-        print str(I)+':', len(list(rooted_trees(I)))
-    
-    A = []
-    f = []
-    for I in rooted_trees(4):
-        A = I[:]
-        f.append(A)
-    print f
-    F = rooted_trees(4)
-    print len(list(combinations_with_replacement(f,4)))
-    print len(list(forests(4)))
-    for I in combinations_with_replacement(F,4):
-        print tuple(I)
         
-    
-
+if __name__ == '__main__':
+    N = sum([4,4,4,3,3])
+    for N in range(8):
+        print str(N)+':', len(list(rooted_trees(N))), len(list(forests(N)))
                     
     
