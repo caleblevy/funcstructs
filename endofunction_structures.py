0f33f033a9d22244df65f48b80d6333f921961be
Caleb Levy <caleb.levy@berkeley.edu>
2014-12-05 11:46:56 -0800

    Finally adding a way to convert structures to canonical functions. Going the other way will likely be trickier, though I suspsect will be of greater interest to others.

--- endofunction_structures.py
+++ endofunction_structures.py
@@ -37,6 +37,49 @@ def structure_multiplicity(function_structure):
     for tree in forest:
         degeneracy *= tree_degeneracy(tree)
     return factorial(n)/degeneracy
+
+def tree_to_func(tree):
+    n = len(tree)
+    height = max(tree)
+    func = range(n)
+    height_prev = 1
+    grafting_point = [None]*height
+    grafting_point[0] = 0
+    for node, height in enumerate(tree[1:]):
+        if height > height_prev:
+            func[node+1] = grafting_point[height_prev-1]
+            height_prev += 1
+        else:
+            func[node+1] = grafting_point[height-2]
+            height_prev = height
+        grafting_point[height-1] = node+1
+    return func
+    
+def canonical_form(function_structure):
+    """Convert function structure to canonical form by filling in numbers from 0 to n-1 on the cycles and trees."""
+    n = len(unpack(unpack(function_structure)))
+    f = range(n)
+    cycle_start = 0
+    unassigned_els = range(n)
+    for cycle in function_structure:
+        node_ind = node_next = 0
+        cycle_len = len(unpack(cycle))
+        for tree in cycle:
+            node_next += len(tree)
+            f[cycle_start + node_ind] = cycle_start + node_next%cycle_len
+            unassigned_els.remove(cycle_start + node_ind)
+            node_ind += len(tree)
+        cycle_start += cycle_len
+    return f
+
+func = (((1,2,3,),(1,2,2,)),((1,2,),),((1,2,2),(1,),(1,2,2,)))
+print canonical_form(func)
+tree = [1,2,3,4,4,4, 3,4,4,  2,3,3, 2,3]
+print tree_to_func(tree)
+            
+        
+    
+    
     
 class EndofunctionTest(unittest.TestCase):
     # OEIS A001372
