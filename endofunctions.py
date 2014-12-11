a79e0f7e88c51b43c4ece9cb31a34fd3838fabb4
Caleb Levy <caleb.levy@berkeley.edu>
2014-12-10 16:11:41 -0800

    Made endofunctions actually work.

--- endofunctions.py
+++ endofunctions.py
@@ -11,44 +11,39 @@ def image_path_naive(f):
         f = [f_orig[x] for x in f]
     return cardinalities
 
-image_path_naive([0]+range(15))
-
-print image_path_naive([0]+range(-1))
-
 def image_path(f):
+    """
+    Given a list f of length n such that all([I in range(n) for I in f]), 
+    """
     n = len(f)
-    cardinalities = []
+    cardinalities = [len(set(f))]
     f_orig = f[:]
     card_prev = n
-    for it in range(n-1):
+    for it in range(1,n-1):
+        f = [f_orig[x] for x in f]
         card = len(set(f))
         cardinalities.append(len(set(f)))
-        f = [f_orig[x] for x in f]
         if card == card_prev:
             cardinalities.extend([card]*(n-2-it))
             break
         card_prev = card
     return cardinalities
-        
-        
-
-f = [0,0,1,2,3,4,5,6,7,8,9,10,11]
-g = [0,0,0,1,2,3,4,5,6,7,8,9,10,11]
-print image_path(f)
-print image_path_naive(f)
-print image_path(g)
-print image_path_naive(g)
 
 class EndofunctionTest(unittest.TestCase):
     def testImagePath(self):
         """Check various special and degenerate cases, with right index"""
-        node_count = [1,3,5,15]
+        self.assertEqual([1], image_path([0]))
+        self.assertEqual([1], image_path([0,0]))
+        self.assertEqual([1], image_path([1,1]))
+        self.assertEqual([2], image_path([0,1]))
+        self.assertEqual([2], image_path([1,0]))
+        node_count = [2,3,5,15]
         for n in node_count:
             tower = [0] + range(n-1)
             cycle = [n-1] + range(n-1)
             fixed = range(n)
             degen = [0]*n
-            print range(n)[:0:-1]
+            self.assertEqual(len(range(n)[:0:-1]), n-1)
             self.assertEqual(range(n)[:0:-1], image_path(tower))
             self.assertEqual([n]*(n-1), image_path(cycle))
             self.assertEqual([n]*(n-1), image_path(fixed))
@@ -56,6 +51,5 @@ class EndofunctionTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-    pass
         
         
