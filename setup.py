0fe68950229a1b31a8f74d4b939d8ebf864f0ada
Caleb Levy <caleb.levy@berkeley.edu>
2014-12-21 18:00:24 -0800

    Added to setup.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(ext_modules = cythonize(["*.py","PADS/*.py"]))
+setup(ext_modules = cythonize(["*.py","PADS/IntegerPartitions.py","PADS.__init__.py"]))
