- Added funcstruct_imagepath to for direct computation of an endofunction structure's image path. - TODO: Reorganize and fix the tests, so that functions need not be moved to different modules for testing.

endofunction_structures.py
@@ -19,6 +19,7 @@ from itertools import combinations_with_replacement, product
 from sympy.utilities.iterables import multiset_partitions
 from necklaces import necklaces, cycle_degeneracy
 from math import factorial
+import numpy as np
 import unittest
         
 def mset_functions(mset):
@@ -84,8 +85,20 @@ def funcstruct_to_func(function_structure):
         cycle_start += cycle_len
     return func
 
-def funcstruct_imagepath(funcstruct):
-    func = list(flatten(flatten(funcstruct)))
+def funcstruct_imagepath(funcstruct, n=None):
+    forest = list(flatten(funcstruct))
+    if n is None:
+        n = len(list(flatten(forest)))
+    cardinalities = np.array([0]+[0]*(n-2), dtype=object)
+    for tree in forest:
+        cardinalities += 1
+        for subseq in increasing_subsequences(tree):
+            k = len(subseq) - 1
+            k -= 1 if subseq[0] is 1 else 0
+            if k > 0:
+                # Microoptimization: memoize the calls to range
+                cardinalities[:k] += range(k,0,-1)
+    return cardinalities
 
 class EndofunctionStructureTest(unittest.TestCase):
     counts = [0, 1, 3, 7, 19, 47, 130, 343, 951, 2615, 7318, 20491, 57903]

endofunctions.py
@@ -31,7 +31,7 @@ image sizes set can be O(n) (and has a lovely closed form formula).
 """
 
 from endofunction_structures import (funcstructs, funcstruct_degeneracy,
-                                     funcstruct_to_func)
+                                     funcstruct_to_func, funcstruct_imagepath)
 from iteration import product_range, compositions
 from math import factorial
 from necklaces import nCk
@@ -257,6 +257,17 @@ class EndofunctionTest(unittest.TestCase):
             self.assertEqual([n]*(n-1), imagepath(cycle))
             self.assertEqual([n]*(n-1), imagepath(fixed))
             self.assertEqual([1]*(n-1), imagepath(degen))
+    
+    def testPaths(self):
+        N = 8
+        for n in range(1,N):
+            for struct in funcstructs(n):
+                try:
+                    np.testing.assert_array_equal(imagepath(funcstruct_to_func(struct)),funcstruct_imagepath(struct))
+                except AssertionError:
+                       print struct
+                       raise
+                
     
     def testIterateImagedist(self):
         """Check the multiplicities of sizes of images of iterates."""