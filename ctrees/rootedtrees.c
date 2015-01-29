5d8e8cd47e467666d5e239e35069340fb2671662
Caleb Levy <caleb.levy@berkeley.edu>
2015-01-29 14:25:57 -0800

    Added function to add one to each element of a tree.

--- ctrees/rootedtrees.c
+++ ctrees/rootedtrees.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void addone(int *t, int len)
+{
+    for(int i = 0; i < len; i++){
+        t[i] += 1;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     int n;
@@ -10,10 +17,13 @@ int main(int argc, char *argv[])
         n = atoi(argv[1]);
     }
     printf("Producing rooted trees on %d nodes\n", n);
-    
+
     int tree[n];
     for(int i = 0; i < n; i++){
         tree[i] = i+1;
+    }
+    addone(tree, n);
+    for(int i = 0; i < n; i++){
         printf("tree[%d]: %d\n", i, tree[i]);
     }
 }
