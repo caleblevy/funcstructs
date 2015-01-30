7dae28473ff1a2a3868d545f7b61aec750d09155
Caleb Levy <caleb.levy@berkeley.edu>
2015-01-29 17:41:35 -0800

    rootedtrees.c now produces and prints the rooted trees.

--- ctrees/rootedtrees.c
+++ ctrees/rootedtrees.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void addone(int *t, int len)
+void printtree(int *tree, int len);
+void successortree(int *tree, int n);
+
+void printtree(int *tree, int len)
 {
-    for(int i = 0; i < len; i++){
-        t[i] += 1;
+    printf("[");
+
+    for(int i = 0; i < len-1; i++) {
+        printf("%d, ", tree[i]);
+    }
+
+    printf("%d]\n", tree[len-1]);
+}
+
+void successortree(int *tree, int n) {
+    int p = n-1;
+    while(tree[p] == tree[1]) {
+        p--;
+    }
+    int q = p-1;
+    while(tree[q] >= tree[p]) {
+        q--;
+    }
+    for(int i = p; i < n; i++) {
+        tree[i] = tree[i-(p-q)];
     }
 }
 
@@ -16,14 +37,30 @@ int main(int argc, char *argv[])
     } else {
         n = atoi(argv[1]);
     }
+    if(n < 1) {
+        printf("ERROR: A tree requires at least one node.\n");
+        return 1;
+    }
     printf("Producing rooted trees on %d nodes\n", n);
 
     int tree[n];
     for(int i = 0; i < n; i++){
         tree[i] = i+1;
     }
-    addone(tree, n);
-    for(int i = 0; i < n; i++){
-        printf("tree[%d]: %d\n", i, tree[i]);
+
+    if(n == 1) {
+        printtree(tree, n);
+        return 0;
+    } else if(n == 2) {
+        printtree(tree, n);
+        return 0;
     }
+    
+    printtree(tree, n);
+    while(tree[1] != tree[2]) {
+        successortree(tree, n);
+        // printtree(tree, n);
+    }
+
+    return 0;
 }
