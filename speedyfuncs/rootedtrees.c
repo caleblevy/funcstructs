1b5eb1be5e8ec73e7a78cde1a47a416fa4c4c25a
Caleb Levy <caleb.levy@berkeley.edu>
2015-01-31 15:17:54 -0800

    Separated rooted tree enumeration from main and the parsing logic.

--- speedyfuncs/rootedtrees.c
+++ speedyfuncs/rootedtrees.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 void printtree(int *tree, int len);
+int *firsttree(int n);
 void successortree(int *tree, int n);
 
 void printtree(int *tree, int len)
@@ -15,7 +16,17 @@ void printtree(int *tree, int len)
     printf("%d]\n", tree[len-1]);
 }
 
-void successortree(int *tree, int n) {
+int *first_tree(int n)
+{
+    int *tree = malloc(n);
+    for(int i = 0; i < n; i++){
+        tree[i] = i+1;
+    }
+    
+    return tree;
+}
+
+void next_tree(int *tree, int n) {
     int p = n-1;
     while(tree[p] == tree[1]) {
         p--;
@@ -29,41 +40,51 @@ void successortree(int *tree, int n) {
     }
 }
 
-int main(int argc, char *argv[])
+void enumerate_trees(int n, int printout)
 {
-    int n;
-    if(argc < 2){
-        n = 0;
-    } else {
-        n = atoi(argv[1]);
-    }
     if(n < 1) {
         printf("ERROR: A tree requires at least one node.\n");
-        return 1;
+        return;
     }
-    printf("Producing rooted trees on %d nodes\n", n);
+    if(printout) printf("Producing rooted trees on %d nodes\n", n);
 
-    int tree[n];
-    for(int i = 0; i < n; i++){
-        tree[i] = i+1;
-    }
+    int *tree = first_tree(n);
 
     if(n == 1) {
-        printtree(tree, n);
+        if(printout) printtree(tree, n);
         printf("There is 1 tree on 1 node.\n");
-        return 0;
     } else if(n == 2) {
-        printtree(tree, n);
+        if(printout) printtree(tree, n);
         printf("There is 1 tree on 2 nodes.\n");
-        return 0;
-    }
-    long count = 1;
-    printtree(tree, n);
-    while(tree[1] != tree[2]) {
-        successortree(tree, n);
-        printtree(tree, n);
+    } else {
+        long count = 0;
+        for(; tree[1] != tree[2]; next_tree(tree, n)) {
+            if(printout) printtree(tree, n);
+            count++;
+        }
         count++;
+        if(printout) printtree(tree, n);
+        printf("There are %ld trees on %d nodes.\n", count, n);
     }
-    printf("There are %ld trees on %d nodes.\n", count, n);
+    free(tree);
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+    int n, printout;
+    if(argc < 2){
+        n = 0;
+        printout = 0;
+    } else {
+        n = atoi(argv[1]);
+        if (argc < 3) {
+            printout = 0;
+        } else {
+            printout = atoi(argv[2]);
+        }
+    }
+    enumerate_trees(n, printout);
+
     return 0;
 }
