542238431e8843541feb0ae81e667d4af721c3fc
Caleb Levy <caleb.levy@berkeley.edu>
2015-02-01 11:27:55 -0800

    Added guarding against special cases to enumerate_fixed_lex_partitions in levypartitions.c.

--- speedyfuncs/levypartitions.c
+++ speedyfuncs/levypartitions.c
@@ -22,7 +22,7 @@ void print_partition(FixedLengthPartition *part)
  * Set the tail end of partition *part to the minimal partition of length n and
  * size L.
  */
-void set_end_partition(FixedLengthPartition *part, int n, int L)
+void minimize_partition_tail(FixedLengthPartition *part, int n, int L)
 {
     int binsize = n / L;
     int overstuffed = n - L*binsize;
@@ -37,7 +37,7 @@ void set_end_partition(FixedLengthPartition *part, int n, int L)
     }
 }
 
-FixedLengthPartition *minimal_partition(int n, int L)
+FixedLengthPartition *first_partition(int n, int L)
 {
     FixedLengthPartition *part = malloc(sizeof(FixedLengthPartition));
     assert(part != NULL);
@@ -49,11 +49,36 @@ FixedLengthPartition *minimal_partition(int n, int L)
     for(int i = 0; i < L; i++) {
         part->comps[i] = 0;
     }
-    set_end_partition(part, n, L);
+    minimize_partition_tail(part, n, L);
     return part;
+}
+
+void enumerate_fixed_lex_partitions(int n, int L)
+{
+    if(!L) {
+        if(!n) {
+            printf("[]\n");
+            printf("There is 1 partition of n=0 into L=0 parts.\n");
+        } else {
+            printf("There are 0 partitions of n=%d into L=0 parts.\n", n);
+        }
+        return;
+
+    } else if(L == 1) {
+        if(n > 0) {
+            printf("[%d]\n", n);
+            printf("There is 1 partition of n=%d into L=1 part.\n", n);
+        } else {
+            printf("There are 0 partitions of n=0 into L=1 part.\n");
+        }
+        return;
+    } else if(n < L) {
+        printf("There are 0 partitions of n=%d into L=%d parts.\n", n, L);
+    }
 }
 
 int main(int argc, char *argv[])
 {
-    print_partition(minimal_partition(10, 4));
+    print_partition(first_partition(10, 4));
+    enumerate_fixed_lex_partitions(0,4);
 }
