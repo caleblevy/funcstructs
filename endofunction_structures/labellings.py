25c161fd6df3a30fb304e763ca666333bfd97c07
Caleb Levy <caleb.levy@berkeley.edu>
2015-02-21 14:45:25 -0800

    Generalized labellings module to evenly divide sets into any number of bins.

--- endofunction_structures/labellings.py
+++ endofunction_structures/labellings.py
@@ -1,74 +1,71 @@
+#!/usr/bin/env python
+# Copyright (C) 2015 Caleb Levy - All Rights Reserved.
+#
+# The terms of use, license and copyright information for the code and ideas
+# contained herein are described in the LICENSE file included with this
+# project. For more information please contact me at caleb.levy@berkeley.edu.
+
 import math
 import itertools
 import unittest
 
-import multiset
+
+def arrangement_count(n, b):
+
+    """The total number of ways of evenly dividing a set S with n elements
+    into b parts is n!/((n/b)!^b b!) which we can see as follows:
+
+    Take all the permutations of S of which there are n!. We divide them
+    into subsequences of length b. If we then disregard ordering each
+    selection of subsequences will appear b! times. Each subsequence in
+    each permutation of subsequences has (n/b)! representations, and there
+    are b independent bins in each selection.
+
+    Each combination of combinations will thus have (n/b)!**b * b! distinct
+    representations. """
+
+    return math.factorial(n)//(math.factorial(n//b)**b)//math.factorial(b)
 
 
-def evenly_split_by_filtering(s, b):
-    """Evenly split S into b parts. Very slow; for testing only."""
-    S = multiset.Multiset(s)
-    n = len(S)
-    if not n == len(S):
-        raise ValueError("We must split a set, not multiset")
-    if n % b:
-        raise ValueError("cannot divide evenly")
-    ls = [n//b]*b
-    for mpart in S.partitions():
-        if [len(el) for el in mpart] == ls:
-            yield frozenset(frozenset(m) for m in mpart)
+def _evenly_divide(items, b):
+    if b == 1:
+        yield [frozenset(items)]
+    else:
+        n = len(items)
+        marked_el = (items[0], )
+        items = items[1:]
+        S = set(items)
+        for first_combo in itertools.combinations(items, n//b-1):
+            remaining_elements = list(S - set(first_combo))
+            for remaining_combos in _evenly_divide(remaining_elements, b-1):
+                yield [frozenset(marked_el + first_combo)] + remaining_combos
 
 
-def split_into_halves(items):
-    """ Split a set into two; i.e. multiset partitions of items into two equal
-    sized bins. """
+def evenly_divide(items, b):
+    """Evenly split a set of itmes into b parts."""
     items = list(items)
-    S = set(items)
-    n = len(items)
-    if n % 2:
-        raise ValueError("needs an even number of items")
-    tot = multiset.nCk(n, n//2)//2
-    count = 0
-    for p in itertools.combinations(items, n//2):
-        count += 1
-        if count > tot:
-            break
-        yield frozenset([frozenset(p), frozenset(S - set(p))])
-
-
-def split_into_thirds(items):
-    """Split a set into thirds; i.e. multiset partitions of items into three
-    equally sized bins."""
-    items = list(items)
-    n = len(items)
-    if n % 3:
-        raise ValueError("items must be split into thirds")
-    marked_el = items[0]
-    items = items[1:]
-    S = set(items)
-    for first_combo in itertools.combinations(items, n//3-1):
-        remaining_elements = sorted(list(S - set(first_combo)))
-        for remaining_split in split_into_halves(remaining_elements):
-            yield frozenset([frozenset((marked_el,)+first_combo)] + list(remaining_split))
+    if len(items) % b:
+        raise ValueError("items must divide evenly")
+    for division in _evenly_divide(items, b):
+        yield frozenset(division)
 
 
 class LabellingTests(unittest.TestCase):
 
-    def test_splits(self):
-        """Test sets are split evenly and identical to multiset partitions."""
-        S = range(6)
-        s1 = frozenset(split_into_halves(S))
-        s2 = frozenset(evenly_split_by_filtering(range(6), 2))
-        self.assertEqual(s2, s1)
+    split_sets = [(6, 2), (6, 3), (9, 3), (12, 3), (12, 4)]
 
-        s3 = frozenset(split_into_thirds(S))
-        s4 = frozenset(evenly_split_by_filtering(range(6), 3))
-        self.assertEqual(s3, s4)
+    def test_division_counts(self):
+        """ Check that we produce the correct number of combinations. """
+        for n, b in self.split_sets:
+            self.assertEqual(arrangement_count(n, b),
+                             len(frozenset(evenly_divide(range(n), b))))
 
-        P = range(9)
-        p1 = frozenset(split_into_thirds(P))
-        p2 = frozenset(evenly_split_by_filtering(P, 3))
-        self.assertEqual(p1, p2)
+    def test_division_uniqueness(self):
+        """Make sure each division is a partition of the original set"""
+        for n, b in self.split_sets:
+            for division in evenly_divide(range(n), b):
+                s = set(itertools.chain.from_iterable(division))
+                self.assertEqual(n, len(s))
 
 
 if __name__ == '__main__':
