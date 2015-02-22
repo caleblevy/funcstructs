5dc37d81585149787ac320b9ded15ca398e1e7d4
Caleb Levy <caleb.levy@berkeley.edu>
2015-02-21 19:57:27 -0800

    labellings.py can now partition a set into cycles of a cycle decomposition. Marked completed tasks in the roadmap.

--- endofunction_structures/labellings.py
+++ endofunction_structures/labellings.py
@@ -9,6 +9,44 @@ import math
 import itertools
 import unittest
 
+import multiset
+
+class SetPartition(object):
+    def __init__(self, S):
+        pass
+        # if len(set(S)) != len(S):
+        #  print warning
+        # if isinstance(S, int):
+        #  S = set(range(S))
+
+
+def multinomial_coefficient(partition, n=None):
+    """Number of ordered combinations into the given partition. """
+    if n is None:
+        n = sum(partition)
+    coeff = 1
+    for p in partition:
+        coeff *= multiset.nCk(n, p)
+        n -= p
+    return coeff
+
+
+def _ordered_divisions(S, partition):
+    if len(partition) == 1:
+        yield [frozenset(S)]
+    else:
+        for first_combo in itertools.combinations(S, partition[0]):
+            for remaining_combos in _ordered_divisions(S - set(first_combo), partition[1:]):
+                yield [frozenset(first_combo)] + remaining_combos
+
+
+def ordered_divisions(S, partition):
+    S = set(S)
+    if not len(S) == sum(partition):
+        raise ValueError("partition must sum to size of set")
+    for p in _ordered_divisions(S, partition):
+        yield tuple(p)
+
 
 def arrangement_count(n, b):
 
@@ -27,46 +65,117 @@ def arrangement_count(n, b):
     return math.factorial(n)//(math.factorial(n//b)**b)//math.factorial(b)
 
 
-def _evenly_divide(items, b):
-    if b == 1:
-        yield [frozenset(items)]
+def _even_divisions(S, b):
+    n = len(S)
+    if b == 1 or n == 1:
+        yield [frozenset(S)]
     else:
-        n = len(items)
-        marked_el = (items[0], )
-        items = items[1:]
-        S = set(items)
-        for first_combo in itertools.combinations(items, n//b-1):
-            remaining_elements = list(S - set(first_combo))
-            for remaining_combos in _evenly_divide(remaining_elements, b-1):
+        marked_el = (S.pop(), )
+        for first_combo in itertools.combinations(S, n//b-1):
+            for remaining_combos in _even_divisions(S - set(first_combo), b-1):
                 yield [frozenset(marked_el + first_combo)] + remaining_combos
 
 
-def evenly_divide(items, b):
+def even_divisions(S, b):
     """Evenly split a set of itmes into b parts."""
-    items = list(items)
-    if len(items) % b:
+    S = set(S)
+    if len(S) % b:
         raise ValueError("items must divide evenly")
-    for division in _evenly_divide(items, b):
+    for division in _even_divisions(S, b):
         yield frozenset(division)
+
+
+def partition_division_count(partition, n=None):
+    """The total number of ways of partitioning S with n elements into a given
+    combination of bin sizes [l1, l2, ..., lk] with multiplicities [m1, m2,
+    ..., mk] is given by n!/(l1!^m1 * m1!)/(l2!^m2 * m2!)/.../(lk!^mk * mk!),
+    which may be seen as follows:
+
+    List all permutations of S of which there are n!. Order the partition
+    arbitrarily, and set division markers in each permutation at those
+    locations. The number of appearances of each group of cycles of the same
+    length is covered above. We just multiply these together to get the number
+    of combinations.
+
+    Alternatively it is the cycle index divided by the factorial of each cycle
+    length, including multiplicity, since here permutation order does not
+    matter.
+    """
+    partition = multiset.Multiset(partition)
+    if n is None:
+        n = sum(partition)
+    count = math.factorial(n)
+    for l, m in zip(*partition.split()):
+        count //= math.factorial(l)**m * math.factorial(m)
+    return count
+
+# Just as a side note to myself, there was a moment (today Feb 21 at 6:30 P.M California) when I was looking at the above block of code - (partition_division_count) after having just been examining and comparing it to set_partition.py in sage's project code, and I thought that this function was part of sage's repository; i.e. I thought it could ahve been part of the sage package.
+
+
+def _unordered_partition_divisions(S, partition):
+    lengths, mults = multiset.Multiset(partition).sort_split()
+    # clm[i] is the number of nodes situated in some bin of size l[i].
+    clm = [l*m for l, m in zip(lengths, mults)]
+    for odiv in _ordered_divisions(S, clm):
+        strand = []
+        for m, cbin in zip(mults, odiv):
+            strand.append(_even_divisions(set(cbin), m))
+        for bundle in itertools.product(*strand):
+            yield frozenset(itertools.chain.from_iterable(bundle))
+
+
+def unordered_partition_divisions(partition, S=None):
+    if S is None:
+        S = range(sum(partition))
+    elif isinstance(S, int):
+        S = range(S)
+    else:
+        S = set(S)
+
+    for subset in itertools.combinations(S, sum(partition)):
+        for p in _unordered_partition_divisions(set(subset), partition):
+            yield p
 
 
 class LabellingTests(unittest.TestCase):
 
     split_sets = [(6, 2), (6, 3), (9, 3), (12, 3), (12, 4)]
 
-    def test_division_counts(self):
+    def test_even_division_counts(self):
         """ Check that we produce the correct number of combinations. """
         for n, b in self.split_sets:
             self.assertEqual(arrangement_count(n, b),
-                             len(frozenset(evenly_divide(range(n), b))))
+                             len(frozenset(even_divisions(range(n), b))))
 
-    def test_division_uniqueness(self):
+    def test_even_division_uniqueness(self):
         """Make sure each division is a partition of the original set"""
         for n, b in self.split_sets:
-            for division in evenly_divide(range(n), b):
+            for division in even_divisions(range(n), b):
+                s = set(itertools.chain.from_iterable(division))
+                self.assertEqual(n, len(s))
+    
+    partitions = [[3, 3, 2, 1], [3, 3, 4], [3, 3, 2, 2], [2, 2, 1]]
+
+    def test_ordered_division_counts(self):
+        for partition in self.partitions:
+            self.assertEqual(multinomial_coefficient(partition),
+                             len(set(ordered_divisions(range(sum(partition)), partition))))
+
+    def test_ordered_partition_sums(self):
+        for partition in self.partitions:
+            n = sum(partition)
+            for division in ordered_divisions(range(sum(partition)), partition):
                 s = set(itertools.chain.from_iterable(division))
                 self.assertEqual(n, len(s))
 
+    def test_unordered_partition_divisions(self):
+        for partition in self.partitions:
+            self.assertEqual(partition_division_count(partition),
+                             len(frozenset(unordered_partition_divisions(partition))))
+        self.assertEqual(partition_division_count([3, 3, 2], 9),
+                         len(frozenset(unordered_partition_divisions([3, 3, 2], 9))))
+        self.assertEqual(len(frozenset(unordered_partition_divisions([2, 2], 5))),
+                         len(frozenset(unordered_partition_divisions([2, 2, 1]))))
 
 if __name__ == '__main__':
     unittest.main()
