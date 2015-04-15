5f2c19c4bb85e0b2cd552ba1a96bfbf6c60827ac
Caleb Levy <caleb.levy@berkeley.edu>
2015-04-14 23:11:41 -0700

    timing docstring cleanup

--- prototyping/timing.py
+++ prototyping/timing.py
@@ -4,7 +4,7 @@
 # contained herein are described in the LICENSE file included with this
 # project. For more information please contact me at caleb.levy@berkeley.edu.
 
-"""Timing functions not found in timit."""
+"""Extra timing functions not found in timeit."""
 
 from __future__ import print_function
 
@@ -22,7 +22,7 @@ def call_string(*args, **kwargs):
 
 
 def object_name(ob):
-    """Return name of generator"""
+    """Return name of ob."""
     try:
         return ob.__name__
     except AttributeError:
@@ -30,10 +30,10 @@ def object_name(ob):
 
 
 def iteration_time(gen, *args, **kwargs):
-    """Time to exhaust gen(*args, **kwargs)"""
+    """Time to exhaust gen. If gen is callable, time gen(*args, **kwargs)."""
     printing = kwargs.pop('printing', True)
     call_sig = object_name(gen)
-    if args or kwargs:
+    if callable(gen):
         gen = gen(*args, **kwargs)
         call_sig += call_string(*args, **kwargs)
     ts = time()
@@ -51,3 +51,5 @@ if __name__ == '__main__':
     iteration_time(levypartitions.fixed_lex_partitions(100, 40))
     iteration_time(EndofunctionStructures(12))
     iteration_time(EndofunctionStructures, 12)
+    iteration_time(PartitionForests, [2,2,3,5])
+    iteration_time(productrange.productrange, -2, [2, 2, 2, 3, 5], step=2)
