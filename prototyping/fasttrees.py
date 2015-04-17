d70378ba01dda7196efbe190c704221362b5a33f
Caleb Levy <caleb.levy@berkeley.edu>
2015-04-17 13:32:14 -0700

    Added fasttrees template module to the prototypes

---
+++ prototyping/fasttrees.py
@@ -0,0 +1,9 @@
+"""Development of fast, nonrecursive algorithm for computing the structure of
+an endofunction.
+
+Caleb Levy, 2015.
+"""
+
+from endofunction_structures import *
+
+from .timing import *
