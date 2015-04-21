0366203c910daac39089b7e722c5b347f00886a8
Caleb Levy <caleb.levy@berkeley.edu>
2015-04-20 20:58:09 -0700

    First draft of nonrecursive tree sorting algorithm

--- prototyping/fasttrees.py
+++ prototyping/fasttrees.py
@@ -7,17 +7,17 @@ Caleb Levy, 2015.
 from __future__ import print_function
 
 import collections
+import unittest
 
 from endofunction_structures import *
-
 from .timing import *
 
 
-def node_levels(func, x):
+def node_levels(func, x, ordered=False):
     """Return the levels of all nodes in the subtree attached to x"""
     node_queue = collections.deque([x])
     jump_node = x
-    level_map = {}
+    level_map = {} if not ordered else collections.OrderedDict()
     level = 1
     while node_queue:
         x = node_queue.popleft()
@@ -41,22 +41,91 @@ def tree_sequence(func, x):
     return level_sequence
 
 
-if __name__ == '__main__':
-    t = DominantTree([1, 2, 3, 4, 5, 2, 3, 4, 3])
-    f = Endofunction([i-1 for i in [5, 4, 6, 6, 4, 6, 9, 3, 8]])
-    l = node_levels(f, 5)
-    print(f.tree_form() == t)
-    print('\nlevels:')
-    for i in range(len(f)):
-        print(i, ':', l[i])
-    print(tree_sequence(f, 5))
-    g = randfunc(10000)
-    for x in g.limitset:
-        h = DominantTree(tree_sequence(g, x))
-        k = DominantTree(g._attached_level_sequence(x))
-        if h != k:
-            raise Exception("Wah, it didn't work!")
+def nodekeys(func, node):
+    level_map = node_levels(func, node, ordered=True)
+    node_keys = collections.defaultdict(list)  # sorting key assigned to nodes
+    levels = subsequences.monotone_subsequences(
+        reversed(list(level_map.keys())),
+        lambda x, y: level_map[x] == level_map[y]  # break at level change
+    )
+    previous_level = next(levels)
+    # sort_value will decrease to produce dominant tree
+    sort_value = 1
+    for x in previous_level:
+        node_keys[x] = sort_value  # Top ones all look identical
+    # NOTE: node_keys[node] <==> sorting key for that node
+    for level in levels:
+        # Account for connections from previous level to current
+        for x in previous_level:
+            node_keys[func[x]].append(node_keys[x])
+        # Sort nodes of current level by weight of their connections
+        for y in level:
+            node_keys[y].sort(reverse=True)
+        # Make a sorted list copy, since iteration order matters
+        sorted_nodes = sorted(level, key=node_keys.get)
+        # Make copy of sorting keys; they will be overwritten in the loop
+        sorting_keys = list(map(node_keys.get, sorted_nodes))
+        # Overwrite sorting keys to prevent accumulation of nested lists
+        for run in subsequences.monotone_subsequences(
+                    zip(sorted_nodes, sorting_keys),
+                    lambda x, y: x[1] == y[1]):
+            sort_value += 1
+            for x in run:
+                node_keys[x[0]] = sort_value
+        previous_level = level
+    return node_keys
 
+
+def sorted_tree_sequence(func, x):
+    """Return the level sequence of the tree attached to x from func"""
+    level_map = node_levels(func, x)
+    node_stack = [x]
+    level_sequence = []
+    node_keys = nodekeys(func, x)
+    while node_stack:
+        x = node_stack.pop()
+        level_sequence.append(level_map[x])
+        node_stack.extend(
+            sorted(func.attached_treenodes[x], key=node_keys.get)
+        )
+    return level_sequence
+
+
+class TestTreeSequences(unittest.TestCase):
+
+    def test_sequences(self):
+        g = randfunc(100)
+        for x in g.limitset:
+            self.assertEqual(
+                g.attached_tree(x),
+                DominantTree(tree_sequence(g, x))
+            )
+        t = DominantTree([1, 2, 3, 4, 5, 2, 3, 4, 3])
+        f = Endofunction(Endofunction([4, 3, 5, 5, 3, 5, 8, 2, 7]))
+        l = DominantTree(tree_sequence(f, 5))
+        lsort = OrderedTree(sorted_tree_sequence(f, 5))
+        self.assertEqual(t, l)
+
+    def test_sorting(self):
+        t = DominantTree([1, 2, 3, 4, 5, 2, 3, 4, 3])
+        f = Endofunction(Endofunction([4, 3, 5, 5, 3, 5, 8, 2, 7]))
+        lsort = OrderedTree(sorted_tree_sequence(f, 5))
+        self.assertEqual(t, lsort)
+        g = randfunc(100)
+        for x in g.limitset:
+            # First ensure same tree structure
+            self.assertEqual(
+                g.attached_tree(x),
+                DominantTree(sorted_tree_sequence(g, x))
+            )
+            # Check that the ordering is the same
+            self.assertEqual(
+                g.attached_tree(x),
+                OrderedTree(sorted_tree_sequence(g, x))
+            )
+
+
+def conversion_times(start, stop=None, step=None):
     def cached_func(n):
         f = randfunc(n)
         f.cycles
@@ -77,17 +146,20 @@ if __name__ == '__main__':
         for x in f.limitset:
             f._attached_level_sequence(x)
 
-    def ordered_tree(f):
-        for x in f.limitset:
-            f.attached_tree(x)
-
     mapping_plots(
-        4000,
+        start, stop, step,
         (stack_tree, cached_func),
         (rec_tree, cached_func),
         (level_tree, cached_func),
-        (ordered_tree, cached_func),
         printing=True
     )
-    import matplotlib.pyplot as plt
-    plt.show()
+    show()
+
+
+def ordered_tree(f):
+    for x in f.limitset:
+        f.attached_tree(x)
+
+
+if __name__ == '__main__':
+    unittest.main()
