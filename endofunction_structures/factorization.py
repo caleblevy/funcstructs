725e6dcddf4f21c89d4467103cae41a254349c96
Caleb Levy <caleb.levy@berkeley.edu>
2015-04-30 18:08:43 -0700

    Cleanup of factorization module

--- endofunction_structures/factorization.py
+++ endofunction_structures/factorization.py
@@ -5,12 +5,13 @@ Caleb Levy, 2014 and 2015.
 
 from . import counts, multiset, productrange
 
+__all__ = ["prime_factorization", "prime_divisors", "divisors"]
+
 
 def prime_factorization(n):
-    """
-    Find the prime factorization of an integer n. Code taken directly from
-        "Prime factorization - list" at http://stackoverflow.com/a/16996439.
-    """
+    """Prime factorization of an integer n."""
+    # Code taken directly from "Prime factorization - list" at
+    # http://stackoverflow.com/a/16996439.
     primfac = []
     d = 2
     while d*d <= n:
@@ -20,27 +21,25 @@ def prime_factorization(n):
         d += 1
     if n > 1:
         primfac.append(n)
-    return primfac
+    return multiset.Multiset(primfac)
 
 
 def prime_divisors(n):
-    return list(set(prime_factorization(n)))
+    """Return all prime factors of n"""
+    return prime_factorization(n).items()
 
 
 def _divisor_gen(n):
-    """
-    Find every divisor of an integer n. Code inspired by
-        "What is the best way to get all the divisors of a number" at
-        http://stackoverflow.com/a/171784.
-    """
+    # Refactoring of "What is the best way to get all the divisors of a number"
+    # at http://stackoverflow.com/a/171784.
     factors, powers = multiset.Multiset(prime_factorization(n)).split()
-    # Since the factors are prime, every unique partition of powers represents
-    # a different divisor.
+    # Since factors are prime, each partition of powers is a different divisor.
     for power_combo in productrange.productrange([m+1 for m in powers]):
         yield counts.prod([factors[i]**p for i, p in enumerate(power_combo)])
 
 
 def divisors(n, factors={}):
+    """Return all integer divisors n."""
     if n not in factors:
-        factors[n] = list(_divisor_gen(n))
+        factors[n] = tuple(_divisor_gen(n))
     return factors[n]
