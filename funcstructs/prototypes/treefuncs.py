7b9e456b4f415b0c00dfa2f82aff1818c1574faa
Caleb Levy <caleb.levy@berkeley.edu>
2015-05-08 12:15:26 -0700

    Added funclevels_iterator to treefuncs

--- funcstructs/prototypes/treefuncs.py
+++ funcstructs/prototypes/treefuncs.py
@@ -7,11 +7,22 @@ from __future__ import print_function
 from collections import defaultdict
 
 
-def _graph_tree(graph, root=0, keys=None):
+def levels_from_func(func, root=None):
+    """Return function level iterator"""
+    if root is None:
+        root = next(iter(func.limitset))
+        if len(func.limitset) != 1:
+            raise ValueError("Function structure is not a rooted tree")
+    for level in levels_from_preim(func.acylic_ancestors, root):
+        yield level
+
+
+def levels_from_preim(graph, root=0, keys=None):
     """Return the level sequence of the ordered tree formed such that graph[x]
     are the nodes attached to x."""
+    preim = graph
     if keys is not None:
-        graph = [sorted(attachments, key=keys.get) for attachments in graph]
+        preim = defaultdict(lambda node: sorted(graph[node], key=keys))
     node_stack = [root]
     level = 0
     node_levels = {root: level}
@@ -20,45 +31,42 @@ def _graph_tree(graph, root=0, keys=None):
         level = node_levels[x]
         yield level
         level += 1
-        for y in graph[x]:
+        for y in preim[x]:
             node_stack.append(y)
             # Need to compute even for dominant tree, since levels will change
             node_levels[y] = level
 
 
-def _func_tree(func, root=None):
-    """Return function level iterator"""
-    if root is None:
-        root = next(iter(func.limitset))
-        if len(func.limitset) != 1:
-            raise ValueError("Function structure is not a rooted tree")
-    for level in _graph_tree(func.acyclic_ancestors, root):
-        yield level
-
-
-def _tree_properties(levels):
-    """Return an endofunction corresponding to a sequence of levels"""
+def funclevels_iterator(levels):
+    """Lazily generate the function of a level tree and each node's level"""
     levels = iter(levels)
-    root = height_prev = next(levels)
-
-    level_sequence = [root]
-    height_groups = defaultdict(list)
-    height_groups[0].append(0)
-    func = [0]
-    preim = defaultdict(set)
-    for node, height in enumerate(levels, start=1):
-        level_sequence.append(height)
-        if height > height_prev:
-            func.append(height_groups[height_prev-root][-1])
-            height_prev += 1
+    root = previous_level = next(levels)
+    f = node = 0
+    yield node, previous_level, f  # node, height, and what it's mapped to
+    grafting_point = {0: 0}
+    for node, level in enumerate(levels, start=1):
+        if level > previous_level:
+            f = grafting_point[previous_level-root]
+            previous_level += 1
         else:
-            func.append(height_groups[height-root-1][-1])
-            height_prev = height
-        preim[func[-1]].add(node)
-        height_groups[height-root].append(node)
+            f = grafting_point[level-root-1]
+            previous_level = level
+        yield node, level, f
+        grafting_point[level-root] = node
+
+
+def tree_properties(levels):
+    """Return an endofunction corresponding to a sequence of levels"""
+    func = []
+    height_groups = defaultdict(list)
+    preim = defaultdict(set)
+    for n, l, f in funclevels_iterator(levels):
+        func.append(f)
+        preim[f].add(n)
+        height_groups[l].append(n)
+    preim[0].remove(0)
     return func, preim, height_groups
 
 
-print(_tree_properties([1, 2, 3, 4, 5]))
-print()
-print(_tree_properties(iter([0, 1, 2, 2, 3, 3, 3, 4, 5, 5, 4, 3, 3, 2, 1, 2])))
+t = iter([0, 1, 2, 2, 3, 3, 3, 4, 5, 5, 4, 3, 3, 2, 1, 2])
+print(tree_properties(t))
