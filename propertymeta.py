d0c43829a15b2df106331c844f1e7860bfe00b88
Caleb Levy <caleb.levy@berkeley.edu>
2015-05-17 15:29:41 -0700

    ParameterMeta now inherits from ABCMeta

--- propertymeta.py
+++ propertymeta.py
@@ -1,47 +1,75 @@
+import abc
 
 
 def make_param_getter(key):
     def fget(self):
-        return self.__params[key]
+        return property(self.__params[key])
     return fget
 
 
-class ParameterMeta(type):
+class ParameterMeta(abc.ABCMeta):
     """Given a list of properties in the class definition statement, define a
     list of these properties, add each with a getter and setter from params,
     and add a list of these params."""
 
     def __new__(mcls, name, bases, dct):
-        params = dct.pop('__parameters__', ())
-        for param in params:
-            dct[param] = property(make_param_getter(param))
-        dct['__slots__'] = '__params'
+        dct['__slots__'] = dct.pop('__parameters__', ())
         return super(ParameterMeta, mcls).__new__(mcls, name, bases, dct)
 
 
-class A(object, metaclass=ParameterMeta):
+class A(object):
     __metaclass__ = ParameterMeta
-    __parameters__ = ["n", "m", "k"]
-    def __init__(self):
-        self.__params = {}
-        self.__params['n'] = 1
-        self.__params['m'] = 2
-        self.__params['k'] = 3
 
 
 a = A()
-print(a)
 print(dir(a))
 
-class B(A):
-    __metaclass__ = type
-    __slots__ = 'l'
-    def __init__(self):
-        self.__params = {}
-        self.__params['l'] = 4
+
+class A2(object):
+    __metaclass__ = ParameterMeta
+
+    @abc.abstractmethod
+    def f(self):
+        return 1
 
 
-b = B()
-print(dir(b))
-print(b)
-print(a.m)
+class A3(A2):
+    __parameters__ = ['n', 'm']
+    def f(self):
+        return 2
+
+
+a3 = A3()
+print dir(a3)
+
+
+class Enumerable(object):
+    __metaclass__ = ParameterMeta
+
+    @abc.abstractmethod
+    def __new__(cls, **kwargs):
+        self = super(Enumerable, cls).__new__(cls)
+        for param, val in kwargs.items():
+            setattr(self, param, make_param_getter(val))
+        return self
+
+    @abc.abstractmethod
+    def __iter__(self):
+        return
+        yield
+
+
+class Range(Enumerable):
+    __parameters__ = ("start", "stop")
+
+    def __new__(cls, start, stop=100):
+        return super(Range, cls).__new__(cls, start=start, stop=stop)
+
+    def __iter__(self):
+        return iter(range(self.start, self.stop))
+
+
+r = Range(20)
+print dir(r)
+r.start=20
+print list(r)
