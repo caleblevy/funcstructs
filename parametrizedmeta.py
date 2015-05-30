f8d7a7eedff3f59bdd5141d2cc19ecb9066e8dab
Caleb Levy <caleb.levy@berkeley.edu>
2015-05-29 19:37:27 -0700

    remove skeleton methods

--- parametrizedmeta.py
+++ parametrizedmeta.py
@@ -7,36 +7,6 @@ import inspect
 import unittest
 
 
-def add_hash(cls, params):
-    def __hash__(self):
-        pass
-    cls.__hash__ = __hash__
-
-
-def add_repr(cls, params):
-    def __repr__(self):
-        pass
-    cls.__repr__ = __repr__
-
-
-def add_eq(cls, params):
-    def __eq__(self, other):
-        pass
-    cls.__eq__ = __eq__
-
-
-def add_setattr(cls, params):
-    def __setattr__(self, name, val):
-        pass
-    cls.__setattr__ = __setattr__
-
-
-def add_delattr(cls, params):
-    def __delattr__(self, name):
-        pass
-    cls.__delattr__ = __delattr__
-
-
 class ParametrizedMeta(type):
     """Metaclass which parametrizes a class by the parameters of it's __init__
     method. If __init__ takes no arguments, the class is assumed to have no
