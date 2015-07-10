fd2c56bc077cdb62c318acb21e53d12fbbb4a8f5
Caleb Levy <caleb.levy@berkeley.edu>
2015-07-09 21:47:41 -0700

    cleaned up tests.__main__

--- tests/__main__.py
+++ tests/__main__.py
@@ -7,31 +7,23 @@ import pkgutil
 import sys
 import unittest
 
-# sys.path hackery which seems to make this run in python and python3
 test_dir = os.path.dirname(os.path.abspath(__file__))
-funcstructs_dir = os.path.dirname(os.path.abspath(test_dir))
-sys.path.insert(0, funcstructs_dir)
-
+# sys.path hackery which seems to make this run in python and python3
+sys.path.insert(0, os.path.dirname(os.path.abspath(test_dir)))
 
 numpy_dependant = []
 matplotlib_dependant = []
 
-
-# For the hackery required to do this, please see:
-# * "how to run all Python unit tests in a directory"
-#   at http://stackoverflow.com/a/1732477/3349520
-# * "Is there a standard way to list names of Python modules in a package?"
-#   at http://stackoverflow.com/a/1310912/3349520
-# * "import module from string variable"
-#   at http://stackoverflow.com/a/8719100/3349520
 suite = unittest.TestSuite()
 
-
+# "Is there a standard way to list names of Python modules in a package?" at
+# http://stackoverflow.com/a/1310912/3349520
 for _, mod, _ in pkgutil.walk_packages([test_dir]):
     if mod == '__main__':
-        continue  # do not test the tester
-    name = 'tests.' + mod
+        continue  # pypy tests __main__ without this
     try:
+        # "import module from string variable" at
+        # http://stackoverflow.com/a/8719100/3349520
         importlib.import_module(mod)
     except ImportError as e:
         error_message = str(e)
@@ -42,15 +34,15 @@ for _, mod, _ in pkgutil.walk_packages([test_dir]):
         else:
             raise
     else:
+        # "how to run all Python unit tests in a directory" at
+        # http://stackoverflow.com/a/1732477/3349520
         suite.addTest(unittest.defaultTestLoader.loadTestsFromName(mod))
-
 
 if numpy_dependant:
     print("\nThe following tests require numpy, and will be skipped:\n")
     for test in numpy_dependant:
         print('\t%s' % test)
     print()
-
 
 if matplotlib_dependant:
     print("\nThe following tests require matplotlib, and will be skipped:\n")
