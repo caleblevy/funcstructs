dd143fce85c76701cf32584a36d3a75ac3487e4b
Caleb Levy <caleb.levy@berkeley.edu>
2015-07-16 17:21:23 -0700

    added binary operations to prototype Multiset

--- funcstructs/prototypes/multiset.py
+++ funcstructs/prototypes/multiset.py
@@ -10,8 +10,84 @@ from collections import Counter, Mapping
 from frozendict import frozendict, _map_get, _map_set
 
 
+def _binop_template(name, map_get, map_set):
+    """Template for wrapping binary operations from Counter to Multiset"""
+    counter_op = getattr(Counter, name)
+
+    def binop(self, other):
+        if isinstance(other, Multiset):
+            other = map_get(other)
+        result = object.__new__(Multiset)
+        map_set(result, counter_op(map_get(self), other))
+        return result
+    binop.__name__ = name
+    binop.__doc__ = counter_op.__doc__.replace("Counter", "Multiset").replace(
+        "counter", "multiset")
+    return binop
+
+
+def _rop_template(name, map_get, map_set):
+    """Make reversed binary ops for Multiset using Counter methods."""
+    binop = getattr(Counter, '__'+name[3:])
+
+    def rop(self, other):
+        return binop(other, map_get(self))
+    rop.__name__ = name
+    rop.__doc__ = None
+    return rop
+
+
+def _MultisetHelper(ms_cls, map_get=_map_get, map_set=_map_set):
+    """Add Counter wrappers to Multiset."""
+
+    def __new__(*args, **kwargs):  # signature allows using `cls` keyword arg
+        """Create a new Multiset. If given, count elements from an
+        input iterable, otherwise initialize the count from another
+        mapping of elements to their multiplicities.
+
+        >>> m = Multiset()                      # a new, empty Multiset
+        >>> m = Multiset('gallahad')            # Multiset from an iterable
+        >>> m = Multiset({'a': 4, 'b': 2})      # Multiset from a mapping
+        >>> m = Multiset(a=4, b=2)              # Multiset from keyword args
+        """
+        self = object.__new__(args[0])
+        mset = Counter()
+        check = False
+        if len(args) == 2:
+            if kwargs:
+                raise TypeError("Multiset does not accept iterable and kwargs")
+            iterable = args[1]
+            if isinstance(iterable, Mapping):
+                dict.update(mset, iterable)
+                check = True
+            else:
+                for el in iterable:
+                    dict.__setitem__(mset, el, mset.get(el, 0) + 1)
+        elif kwargs:
+            dict.update(mset, kwargs)
+            check = True
+        elif len(args) > 2:
+            raise TypeError("expected at most 2 arguments, got %d" % len(args))
+        if check:
+            if not all((isinstance(v, int) and v > 0) for v in mset.values()):
+                raise TypeError("multiplicities must be positive integers")
+        map_set(self, mset)
+        return self
+
+    ms_cls.__new__ = staticmethod(__new__)
+
+    for op in ['__add__', '__sub__', '__and__', '__or__']:
+        setattr(ms_cls, op, _binop_template(op, map_get, map_set))
+
+    for rop in ['__radd__', '__rsub__', '__rand__', '__ror__']:
+        setattr(ms_cls, rop, _rop_template(rop, map_get, map_set))
+
+    return ms_cls
+
+
 # Internally store Counter instead of dict, both for speed and for easier
 # interaction with addition methods in python2.
+@_MultisetHelper
 class Multiset(frozendict):
     """frozendict subclass for counting hashable items.  Multiset is the
     immutable counterpart to collections.Counter.  Elements are stored as
@@ -84,46 +160,6 @@ class Multiset(frozendict):
     most_common.__doc__ = most_common.__doc__.replace("Counter", "Multiset")
 
 
-def _MultisetHelper(ms_cls, map_get=_map_get, map_set=_map_set):
-    """Add Counter wrappers to Multiset."""
-
-    def __new__(*args, **kwargs):  # signature allows using `cls` keyword arg
-        """Create a new Multiset. If given, count elements from an
-        input iterable, otherwise initialize the count from another
-        mapping of elements to their multiplicities.
-
-        >>> m = Multiset()                      # a new, empty Multiset
-        >>> m = Multiset('gallahad')            # Multiset from an iterable
-        >>> m = Multiset({'a': 4, 'b': 2})      # Multiset from a mapping
-        >>> m = Multiset(a=4, b=2)              # Multiset from keyword args
-        """
-        self = object.__new__(args[0])
-        mset = Counter()
-        check = False
-        if len(args) == 2:
-            if kwargs:
-                raise TypeError("Multiset does not accept iterable and kwargs")
-            iterable = args[1]
-            if isinstance(iterable, Mapping):
-                dict.update(mset, iterable)
-                check = True
-            else:
-                for el in iterable:
-                    dict.__setitem__(mset, el, mset.get(el, 0) + 1)
-        elif kwargs:
-            dict.update(mset, kwargs)
-            check = True
-        elif len(args) > 2:
-            raise TypeError("expected at most 2 arguments, got %d" % len(args))
-        if check:
-            if not all((isinstance(v, int) and v > 0) for v in mset.values()):
-                raise TypeError("multiplicities must be positive integers")
-        map_set(self, mset)
-        return self
-
-    ms_cls.__new__ = staticmethod(__new__)
-
-
 _MultisetHelper(Multiset)
 
 
@@ -142,3 +178,17 @@ print(list(abra))
 print(len(abra))
 print(abra.num_unique_elements())
 print(len(abra.viewitems()))
+
+
+a = Multiset("abracadabra")
+b = Multiset(range(3))
+c = Counter(a)
+d = Counter(b)
+
+# print(a, b, c, d)
+print(a + b)
+print(a + c)
+print(a - c)
+print(c - a)
+print(repr(c + a))
+print(type(c + a))
