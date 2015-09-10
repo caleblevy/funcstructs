f87000184cde43019039daecb83075a7f5e88179
Caleb Levy <caleb.levy@berkeley.edu>
2015-09-09 22:12:20 -0400

    clearpycache.sh now clears .pyo files

--- clearpycache.sh
+++ clearpycache.sh
@@ -3,8 +3,9 @@ echo "Clearing the following python cache files:"
 echo "------------------------------------------"
 find . -name "*.pyc"
 find . -name "*.pyc" -delete
+find . -name "*.pyo"
+find . -name "*.pyo" -delete
 find . -name "*.class"
 find . -name "*.class" -delete
-
 echo "------------------------------------------"
 echo ""
