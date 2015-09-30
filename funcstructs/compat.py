5e5ea71b8ae2a5a27e33e6a42df51407bdef3885
Caleb Levy <caleb.levy@berkeley.edu>
2015-09-29 21:59:59 -0400

    removed optional six dependency

--- funcstructs/compat.py
+++ funcstructs/compat.py
@@ -20,15 +20,14 @@ except ImportError:
             total = func(total, element)
             yield total
 
-try:  # prefer maintained version when available
-    from six import with_metaclass
-except ImportError:
-    def with_metaclass(meta, *bases):
-        """Create a base class with a metaclass."""
-        # This requires a bit of explanation: the basic idea is to make a dummy
-        # metaclass for one level of class instantiation that replaces itself
-        # with the actual metaclass.
-        class metaclass(meta):
-            def __new__(cls, name, this_bases, d):
-                return meta(name, bases, d)
-        return type.__new__(metaclass, 'temporary_class', (), {})
+
+# Copied directly from the six compatibility module
+def with_metaclass(meta, *bases):
+    """Create a base class with a metaclass."""
+    # This requires a bit of explanation: the basic idea is to make a dummy
+    # metaclass for one level of class instantiation that replaces itself
+    # with the actual metaclass.
+    class metaclass(meta):
+        def __new__(cls, name, this_bases, d):
+            return meta(name, bases, d)
+    return type.__new__(metaclass, 'temporary_class', (), {})
